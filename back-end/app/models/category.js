85912dec0dc512599c7c6af220afdddaa73175ba
salahSurvivor <106560141+salahSurvivor@users.noreply.github.com>
2025-09-26 14:50:12 +0100

    Delete back-end directory

--- back-end/app/models/category.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const categorySchema = mongoose.Schema({
-    name: {type: String},
-});
-
-const category = mongoose.model('categorys', categorySchema);
-module.exports = category;
--- back-end/app/models/cv.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const cvSchema = mongoose.Schema({
-    name: {type: String}
-})
-
-const cv = mongoose.model('cv', cvSchema);
-module.exports = cv;
